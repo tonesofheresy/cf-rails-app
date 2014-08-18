4389914050f97a0b19eb243f1aa1f862b298f46d
Sunny Mittal <86.s.mittal@gmail.com>
2014-08-18 01:00:32 -0600

    Create new user form

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+	validates :first, presence: { message: "name can't be empty" }
+	validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+end
